e4a34ef71a024c2229c5118340257d269b691cd6
plamenbelevm <kasa6teka@gmail.com>
2018-08-31 00:14:23 +0300

    exer

--- AJAX_and_jQuery_AJAX/messenger/solution.js
+++ AJAX_and_jQuery_AJAX/messenger/solution.js
@@ -1,17 +1,77 @@
 function attachEvents(){
-    //let author = $('#author').val();
-    //let content = $('#content').val();
-    //let timestamp = Date.now();
-    
-    let request = {
-        url : 'https://messenger-efc06.firebaseio.com/.json',
-        success : displaymessages
-
-    };
-    $.ajax(request);
-    function displaymessages(messages) {
-        for (const message in messages) {
-            console.log(key + ' ' + messages[key]);
+    let url = 'https://messenger-efc06.firebaseio.com/.json';
+    $('#submit').click(submit);
+    $('#refresh').click(refresh);
+    let err;
+    function submit(){
+        let author = $('#author').val();
+        let content = $('#content').val();
+        let timestamp = Date.now();
+        let body;
+        if (author.length === 0) {
+            err = $(`<div>Name can't be empty!</div>`).appendTo($('#main'));
+            setTimeout(function(){ err.text(''); }, 3000);
+            return;
         }
+        if (content.length === 0) {
+            err = $(`<div>Content can't be empty!</div>`).appendTo($('#main'));
+            setTimeout(function(){ err.text(''); }, 3000);
+            return;
+        }
+            
+        body = {
+            author,
+            content,
+            timestamp
+        };
+        
+        let request = {
+            url : url,
+            method : 'POST',
+            data : JSON.stringify(body),
+            success : displayMessages,
+            error : dispalyError
+        };
+        $.ajax(request);
+        $('#author').val('');
+        $('#content').val('');
     }
+    function refresh(){
+        let request = {
+            url : url,
+            success : displayMessages,
+            error : dispalyError
+        };
+        $.ajax(request);
+    }
+    
+    function displayMessages(){
+        let request = {
+            url : url,
+            success : (data)=> {
+                let posts = [];
+                for (const key in data) {
+                    posts.push(data[key]);
+                }
+                posts.sort( (x,y)=> {
+                    return x.timestamp - y.timestamp;
+                });
+                let board = posts.map(x => `${x['author']}: ${x['content']}`).join('\n');
+                //for (const post of posts) {
+                //    //board.append((`<p>${post['author']}: ${post['content']}</p>`));
+                //    //$('#messages').val(`${post['author']}: ${post['content']}`);
+                //    board = 
+                //    console.log(Object.keys(post));
+                //}
+                $('#messages').val(board);
+            }
+        };
+        $.ajax(request);
+    }
+    function dispalyError(err){
+        //console.log(err);
+        let error = $(`<div>${err.statusText}</div>`).appendTo($('#main'));
+        setTimeout(function(){ error.text(''); }, 3000);
+    }
+    
 }
---
+++ AJAX_and_jQuery_AJAX/phones/phones.js
@@ -0,0 +1,65 @@
+function attachEvents(){
+    let baseUrl = 'https://paco-phones.firebaseio.com/';
+    $('#btnLoad').click(loadPhones);
+    $('#btnCreate').click(createEntry);
+    loadPhones();
+    let error;
+    function loadPhones(){
+        let list = $('#phonebook');
+        list.empty();
+        let request = {
+            url : baseUrl + '.json',
+            success : (data) => {
+                for (const person in data) {
+                    list.append($(`<li>${data[person]['name']}: ${data[person]['phone']}</li>`).append($('<button>Delete</button>').on('click', deleteEntry.bind(person)) ) );
+                }
+            },
+            error : displayError
+        };
+        $.ajax(request);
+
+    }
+    function deleteEntry(){
+
+        let url = `${baseUrl}${this}.json`;
+        let request = {
+            url : url,
+            method : 'DELETE',
+            success : loadPhones,
+            error : displayError
+        };
+        $.ajax(request);
+    }
+    function createEntry(){
+        let url = baseUrl + '.json';
+        let name = $('#person').val();
+        let phone = $('#phone').val();
+        if (name.length === 0) {
+            error = $(`<div>Name can not be empty!</div>`).appendTo($('html'));
+            setTimeout(function(){ error.text(''); }, 3000);
+            return;
+        }
+        if (phone.length === 0) {
+            error = $(`<div>Phone can not be empty!</div>`).appendTo($('html'));
+            setTimeout(function(){ error.text(''); }, 3000);
+            return;
+        }
+        let body = {
+            name ,
+            phone
+        };
+        let request = {
+            url : url,
+            method : 'POST',
+            data : JSON.stringify(body),
+            success : loadPhones,
+            error : displayError,
+        };
+        $.ajax(request);
+    }
+    function displayError(err){
+        error = $(`<div>${err.statusText}</div>`).appendTo($('html'));
+        setTimeout(function(){ error.text(''); }, 3000);
+    }
+    
+}
